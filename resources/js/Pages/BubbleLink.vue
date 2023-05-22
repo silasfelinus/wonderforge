341243f72c546470c77efe8ddd66057463e77eb7
silasfelinus <silasfelinus@gmail.com>
2023-05-21 20:10:35 -0700

    NavMenu Works!

--- resources/js/Pages/BubbleLink.vue
+++ resources/js/Pages/BubbleLink.vue
@@ -1,11 +1,13 @@
 <template>
     <div
-      class="bubble flex flex-col items-center justify-center relative overflow-hidden shadow-lg transform transition-transform duration-500 ease-in-out hover:scale-125 rounded-full"
+      class="bubble-container flex flex-col items-center justify-center relative overflow-visible shadow-lg transition-transform duration-500 ease-in-out"
       @mouseover="hover(index); generateRandomColor();"
       @mouseout="unhover"
     >
-      <img v-if="imageUrl" :src="imageUrl" class="w-full h-full object-cover"/>
-      <span class="mt-1">{{ gallery.name }}</span>
+        <div class="bubble w-24 h-24 rounded-full overflow-hidden absolute">
+            <img v-if="imageUrl" :src="imageUrl" class="w-full h-full object-cover"/>
+            <span class="bubble-label">{{ gallery.name }}</span>
+        </div>
     </div>
 </template>
 
@@ -44,18 +46,25 @@ const unhover = () => {
 
 const emit = defineEmits(['hover', 'unhover'])
 </script>
-
 <style scoped>
-.bubble {
-  border-radius: 50%;
-  width: 100px;
-  height: 100px;
-  animation: float 3s ease-in-out infinite;
+.bubble-container {
+  animation: float 12s ease-in-out infinite;
 }
 
-@keyframes float {
-  0% { transform: translatey(0px); }
-  50% { transform: translatey(-20px); }
-  100% { transform: translatey(0px); }
+.bubble:hover {
+  transform: scale(1.2);
+}
+
+.bubble-label {
+  position: absolute;
+  top: 10px; /* Adjust this value to position the label */
+  width: 100%;
+  text-align: center;
+  font-size: 0.8rem; /* Adjust the font size as needed */
+  color: #fff;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 50%; /* This gives the label a curved background */
+  padding: 5px 0; /* Padding to give the label some room */
 }
 </style>
--- resources/js/Pages/NavDrawer.vue
+++ resources/js/Pages/NavDrawer.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="fixed bottom-0 left-0 w-full z-50" :class="{ 'h-16': settings.navDrawerState === 'collapsed', 'h-40': settings.navDrawerState === 'partial', 'h-60': settings.navDrawerState === 'open' }">
         <button @click="toggleNavDrawer" class="block mx-auto my-2">Toggle</button>
-        <div class="flex justify-around flex-wrap h-1/2 overflow-auto">
+        <div class="flex justify-around flex-wrap h-1/2 mb-2 overflow-visible">
             <div v-for="(item, index) in drawer01Items" :key="index" class="w-1/2 md:w-1/4 lg:w-1/6 xl:w-1/12">
                 <Link :href="item.path">
                   <BubbleLink :gallery="item" :index="index" :hoveredIndex="hoveredIndex" @hover="handleHover(index)" @unhover="handleUnhover"/>
                 </Link>
             </div>
         </div>
-        <div class="flex justify-around flex-wrap h-1/2 overflow-auto mt-4">
+        <div class="flex justify-around flex-wrap h-1/2 overflow-visible">
             <div v-for="(item, index) in drawer02Items" :key="index" class="w-1/2 md:w-1/4 lg:w-1/6 xl:w-1/12">
                 <Link :href="item.path">
                   <BubbleLink :gallery="item" :index="index" @hover="handleHover(index)" @unhover="handleUnhover"/>
@@ -17,6 +17,7 @@
         </div>
     </div>
 </template>
+
 
 <script setup>
 import { ref, onMounted } from 'vue'
